c23477bd86f06f56d6598634c77bc00b4f766c25
Martin Sarrionandia <martin@sarrionandia.co.uk>
2025-05-15 22:51:50 +0100

    fix: correct resource name for spf record

--- route53.tf
+++ route53.tf
@@ -19,7 +19,7 @@ resource "aws_route53_record" "this_amazonses_verification_record" {
   records = [aws_ses_domain_identity.this.verification_token]
 }
 
-resource "aws_route53_record" "this_amazonses_verification_record" {
+resource "aws_route53_record" "this_spf_record" {
   count   = var.create-spf-record ? 1 : 0
   zone_id = data.aws_route53_zone.this.zone_id
   type    = "TXT"
